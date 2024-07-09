30bf4644dd031cfc02f91ecf1fbfc6056ec86522
Msys <mhmd.selawe44@gmail.com>
2024-07-10 00:46:10 +0300

    added database

---
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/Movie.java
@@ -0,0 +1,78 @@
+package il.cshaifasweng.OCSFMediatorExample.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "movies")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "director")
+    private String director;
+
+    @Column(name = "genre")
+    private String genre;
+
+    @Column(name = "release_year")
+    private int releaseYear;
+
+    private int showtime;
+
+    // Constructors, getters, and setters
+
+    public Movie() {
+    }
+
+    public Movie(String title, String director, String genre, int releaseYear) {
+        this.title = title;
+        this.director = director;
+        this.genre = genre;
+        this.releaseYear = releaseYear;
+    }
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+}
---
+++ server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/MovieDAO.java
@@ -0,0 +1,72 @@
+package il.cshaifasweng.OCSFMediatorExample.server;
+
+
+import il.cshaifasweng.OCSFMediatorExample.entities.Movie;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import java.util.List;
+
+public class MovieDAO {
+    private SessionFactory sessionFactory;
+
+    public MovieDAO() {
+        // Initialize the SessionFactory from the configuration file
+        Configuration configuration = new Configuration();
+        configuration.addAnnotatedClass(Movie.class);
+
+        // You can add other annotated classes here
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties()).build();
+
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+    }
+
+    public void addMovie(Movie movie) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(movie);
+        transaction.commit();
+        session.close();
+    }
+
+    public void deleteMovie(int movieId) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Movie movie = session.get(Movie.class, movieId);
+        if (movie != null) {
+            session.delete(movie);
+        }
+        transaction.commit();
+        session.close();
+    }
+
+    public List<Movie> getAllMovies() {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
+        List<Movie> movies = null;
+
+        try {
+            transaction = session.beginTransaction();
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaQuery<Movie> query = builder.createQuery(Movie.class);
+            query.from(Movie.class);
+            movies = session.createQuery(query).getResultList();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return movies;
+    }
+}
